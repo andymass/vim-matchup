a8d1b8e635d666b19effa842f331ffa32beb57c2
Andy K. Massimino <f8a663@normed.space>
2023-04-29 12:46:54 -0400

    Add TS cpp template arg query

--- after/queries/cpp/matchup.scm
+++ after/queries/cpp/matchup.scm
@@ -3,3 +3,7 @@
 (template_parameter_list
   "<" @open.template
   ">" @close.template) @scope.template
+
+(template_argument_list
+  "<" @open.template_arg
+  ">" @close.template_arg) @scope.template_arg
