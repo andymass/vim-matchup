e54af0782e17c70044814ffa8ef301189e52ee79
Frank <fbearoff@users.noreply.github.com>
2023-05-30 20:41:28 -0400

    Additional bash TS queries

--- after/queries/bash/matchup.scm
+++ after/queries/bash/matchup.scm
@@ -1,5 +1,29 @@
 (if_statement
   "if" @open.if
   "fi" @close.if) @scope.if
+
 (else_clause
   "else" @mid.if.1)
+
+(elif_clause
+  "elif" @mid.if.2)
+
+(while_statement
+  ("while")? @open.loop
+  ("until")? @open.loop
+  (do_group
+    "done" @close.loop)) @scope.loop
+
+(for_statement
+  ("for")? @open.loop
+  ("select")? @open.loop
+  (do_group
+    "done" @close.loop)) @scope.loop
+
+((word) @mid.loop.1 (#eq? @mid.loop.1 "break"))
+((word) @mid.loop.2 (#eq? @mid.loop.2 "continue"))
+
+(case_statement
+  "case" @open.case
+  (case_item) @mid.case.1
+  "esac" @close.case) @scope.case
