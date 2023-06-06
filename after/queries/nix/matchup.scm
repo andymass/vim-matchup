51b5cb3b8c5793779ec14505a6779eba3eaf44a4
Mikhail Katychev <mkatych@gmail.com>
2023-06-06 11:28:33 -0500

    added nix matchup

--- after/queries/nix/matchup.scm
+++ after/queries/nix/matchup.scm
@@ -1,6 +1,9 @@
 ; --------------- let/in ---------------
-(let_expression "let" @open.let (binding_set)
+(let_expression
+  "let" @open.let (binding_set)
   "in" @mid.let.1 (_)) @scope.let
 ; --------------- binding --------------
-; (binding (_)+ (function_exppression) ";") tend to be many lines lone
-(binding (attrpath) @open.binding (function_expression) ";" @close.binding) @scope.binding
+; (binding (_)+ (function_exppression) ";") tend to be many lines long
+(binding
+  (attrpath) @open.binding (function_expression)
+  ";" @close.binding) @scope.binding
