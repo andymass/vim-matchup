1c56fe371e93644441a2ddf656229b2bece887c6
Mikhail Katychev <mkatych@gmail.com>
2023-06-06 11:07:24 -0500

    added nix syntax

---
+++ after/queries/nix/matchup.scm
@@ -0,0 +1,6 @@
+; --------------- let/in ---------------
+(let_expression "let" @open.let (binding_set)
+  "in" @mid.let.1 (_)) @scope.let
+; --------------- binding --------------
+; (binding (_)+ (function_exppression) ";") tend to be many lines lone
+(binding (attrpath) @open.binding (function_expression) ";" @close.binding) @scope.binding
