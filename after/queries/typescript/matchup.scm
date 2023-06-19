3319672cd7b8bd487a94c1284896dfac8aaeaa68
Vadim Kazakov <yads12@gmail.com>
2023-06-19 08:45:31 -0600

    add additional matchup for typescript
    * allow type argument and parameters to be matched up such that `function foo(): Promise<any>` matches up the `<` and `>` characters

--- after/queries/typescript/matchup.scm
+++ after/queries/typescript/matchup.scm
@@ -1,1 +1,9 @@
 ; inherits: ecma
+
+(type_arguments) @scope.typeargs
+(type_arguments) "<" @open.typeargs
+(type_arguments) ">" @close.typeargs
+
+(type_parameters) @scope.typeparams
+(type_parameters) "<" @open.typeparams
+(type_parameters) ">" @close.typeparams
