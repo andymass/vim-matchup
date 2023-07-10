f1093d83b0594435f35cfa9948923c11c6427011
Amaan Qureshi <amaanq12@gmail.com>
2023-07-10 19:20:44 -0400

    feat: add Smali query (#301)

---
+++ after/queries/smali/matchup.scm
@@ -0,0 +1,40 @@
+; inherits: quote
+
+(method_definition 
+  ".method" @open.function
+  ".end method" @close.function) @scope.function
+
+(parameter_directive
+  ".parameter" @open.parameter
+  ".end parameter" @close.parameter) @scope.parameter
+
+(param_directive
+  ".param" @open.parameter
+  ".end param" @close.parameter) @scope.parameter
+
+((opcode) @mid.function.1
+  (#lua-match? @mid.function.1 "^return"))
+
+(annotation_directive
+  ".annotation" @open.annotation
+  ".end annotation" @close.annotation) @scope.annotation
+
+(subannotation_directive
+  ".subannotation" @open.subannotation
+  ".end subannotation" @close.subannotation) @scope.subannotation
+
+(array_data_directive
+  ".array-data" @open.array-data
+  ".end array-data" @close.array-data) @scope.array-data
+
+(field_definition
+  ".field" @open.field
+  ".end field" @close.field) @scope.field
+
+(sparse_switch_directive
+  ".sparse-switch" @open.sparse-switch
+  ".end sparse-switch" @close.sparse-switch) @scope.sparse-switch
+
+(packed_switch_directive
+  ".packed-switch" @open.packed-switch
+  ".end packed-switch" @close.packed-switch) @scope.packed-switch
