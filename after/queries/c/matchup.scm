77d42f7457d0905785b1d58fee013a805b894779
Amaan Qureshi <amaanq12@gmail.com>
2023-07-18 02:04:42 -0400

    fix(c): update queries from upstream

--- after/queries/c/matchup.scm
+++ after/queries/c/matchup.scm
@@ -18,9 +18,15 @@
     (case_statement "default" @mid.switch.2)?)) @scope.switch
 
 ; 'else' and 'else if'
-(if_statement
+(else_clause
   "else" @_start (if_statement "if" @_end)?
   (#make-range! "mid.if.1" @_start @_end))
+
+; if
+((if_statement
+  "if" @open.if) @scope.if
+ (#not-has-parent? @scope.if else_clause))
+
 
 ; if
 (compound_statement
