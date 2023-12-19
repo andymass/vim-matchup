2550178c43464134ce65328da458905f70dbe041
Andy K. Massimino <f8a663@normed.space>
2023-12-19 08:38:33 -0500

    Rewrite TS perl query, fix #334

--- after/queries/perl/matchup.scm
+++ after/queries/perl/matchup.scm
@@ -1,12 +1,11 @@
-; inherits: quote
-
-(if_statement
+(conditional_statement
     "if" @open.if
-    "elsif"? @mid.if.1
-    "else"? @mid.if.2
 ) @scope.if
 
-(function_definition
+(elsif "elsif"? @mid.if.1)
+(else "else"? @mid.if.2)
+
+(subroutine_declaration_statement
     "sub" @open.fun
 ) @scope.fun
 (return_expression "return" @mid.fun.1)
