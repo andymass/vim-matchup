1975afe63198ab6a0dff7200919828e5cd4330b9
Veesh Goldman <rabbiveesh@gmail.com>
2024-09-12 15:53:40 +0300

    feat(perl): add matchup for loops + quotes (#366)
    * feat(perl): add matchup for loops + quotes
    
    * feat(perl): add try/catch matchup

--- after/queries/perl/matchup.scm
+++ after/queries/perl/matchup.scm
@@ -1,11 +1,34 @@
+; matches any conditional -> else type block -> end of final block
 (conditional_statement
-    "if" @open.if
+    ["if" "unless"] @open.if
+    "elsif"? @mid.if.1
+    "else"? @mid.if.2
+    (block "}" @close.if) .
 ) @scope.if
 
-(elsif "elsif"? @mid.if.1)
-(else "else"? @mid.if.2)
+; matches any loop construct -> loop control (last, next) -> end of final block
+(_
+  ["for" "foreach" "while" "unless"] @open.loop
+  (block "}" @close.loop) .
+  ) @scope.loop
+(loopex_expression) @mid.loop.1
 
-(subroutine_declaration_statement
+; matches sub -> return -> end of block
+(_
     "sub" @open.fun
+    (block "}" @close.fun) .
 ) @scope.fun
 (return_expression "return" @mid.fun.1)
+
+; handling for all the different quote types; multi part quotes cycle through
+[
+  (_ "'" @open.quotelike (string_content) "'" @close.quotelike)
+  (quoted_regexp "'" @open.quotelike "'" @close.quotelike)
+  (_ "'" @open.quotelike (_) "'"+ @mid.quotelike.1 (replacement) "'" @close.quotelike)
+] @scope.quotelike
+
+(try_statement
+  "try" @open.try
+  "catch"? @mid.try.1
+  "finally"? @close.try
+  ) @scope.try
